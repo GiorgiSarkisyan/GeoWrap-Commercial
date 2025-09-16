e116e6e9066d6afc778cd9087f8e83c0ecac3682
GiorgiSarkisyan <gios48320@gmail.com>
2025-09-16 22:32:59 +0400

    new changes

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,3 +1,24 @@
-export default function Home() {
-  return <div></div>;
+import Image from "next/image";
+import styles from "../styles/page/page.module.scss";
+
+export default function Page() {
+  return (
+    <div>
+      <header className={styles["header"]}>
+        <Image
+          src="/images/logo-black.png"
+          alt="geo-wrap-logo"
+          width={220}
+          height={80}
+        />
+        <nav className="nav-wrapper">
+          <ul className="list">
+            <li className="list-item">Home</li>
+            <li className="list-item">About</li>
+            <li className="list-item">Contact</li>
+          </ul>
+        </nav>
+      </header>
+    </div>
+  );
 }
