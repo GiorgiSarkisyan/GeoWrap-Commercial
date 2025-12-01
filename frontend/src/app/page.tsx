09dc1becd47f2e7130418ef32dd8da23de62a8f3
GiorgiSarkisyan <gios48320@gmail.com>
2025-12-01 21:48:14 +0400

    feature: added workshop slider

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,15 +1,101 @@
 "use client";
 import Image from "next/image";
 import styles from "../styles/page/page.module.scss";
-import { FaChevronDown, FaEye } from "react-icons/fa";
+import { FaChevronDown } from "react-icons/fa";
 import { useState } from "react";
 import { BsEye } from "react-icons/bs";
 import { CgClose } from "react-icons/cg";
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import "swiper/css/effect-coverflow";
+import { Swiper, SwiperSlide } from "swiper/react";
+import {
+  EffectCoverflow,
+  Navigation,
+  Pagination,
+  Autoplay,
+} from "swiper/modules";
 
 const languages = [
   { code: "en", label: "English", flag: "/images/flags/en.png" },
   { code: "ru", label: "Русский", flag: "/images/flags/ru.png" },
   { code: "ge", label: "ქართული", flag: "/images/flags/ge.png" },
+];
+
+const workshop = [
+  {
+    id: 1,
+    imageUrl: "/images/workshop/audi-rs6.jpg",
+    title: "Audi RS6",
+    type: "High-performance luxury car",
+  },
+  {
+    id: 2,
+    imageUrl: "/images/workshop/bmw-m850.jpg",
+    title: "BMW M850",
+    type: "High-performance luxury grand tourer",
+  },
+  {
+    id: 3,
+    imageUrl: "/images/workshop/chevrolet-corvette-c3.jpg",
+    title: "Chevrolet Corvette C3",
+    type: "Classic American sports car",
+  },
+  {
+    id: 4,
+    imageUrl: "/images/workshop/ferrari-296gtb.jpg",
+    title: "Ferrari 296GTB",
+    type: "Supercar",
+  },
+  {
+    id: 5,
+    imageUrl: "/images/workshop/lamborghini-urus-performante.jpg",
+    title: "Lamborghini Urus Performante",
+    type: "High-performance luxury SUV",
+  },
+  {
+    id: 6,
+    imageUrl: "/images/workshop/mercedes-sl190.jpg",
+    title: "Mercedes SL190",
+    type: "Classic luxury roadster",
+  },
+  {
+    id: 7,
+    imageUrl: "/images/workshop/mercedes-v300.jpg",
+    title: "Mercedes V300",
+    type: "Luxury van",
+  },
+  {
+    id: 8,
+    imageUrl: "/images/workshop/mercedes-gt63.jpg",
+    title: "Mercedes GT63",
+    type: "High-performance luxury sports sedan",
+  },
+  {
+    id: 9,
+    imageUrl: "/images/workshop/porsche-911-carrera.jpg",
+    title: "Porsche 911 Carrera",
+    type: "High-performance luxury sports car",
+  },
+  {
+    id: 10,
+    imageUrl: "/images/workshop/porsche-911-carrera-gts.jpg",
+    title: "Porsche 911 Carrera GTS",
+    type: "High-performance luxury sports car",
+  },
+  {
+    id: 11,
+    imageUrl: "/images/workshop/porsche-gt3.jpg",
+    title: "Porsche GT3",
+    type: "Track-focused sports car",
+  },
+  {
+    id: 12,
+    imageUrl: "/images/workshop/shelby-cobra.jpg",
+    title: "Shelby Cobra",
+    type: "Classic American sports car",
+  },
 ];
 
 const services = [
@@ -311,6 +397,58 @@ export default function Page() {
           </div>
         </>
       )}
+      <section className={styles["workshop-section"]}>
+        <div className={styles["workshop-header"]}>
+          <h2 className={styles["workshop-section-title"]}>Workshop</h2>
+          <span className={styles["workshop-section-second-title"]}>
+            Check out our most impressive works
+          </span>
+        </div>
+        <div className={styles["workshop-content"]}>
+          <Swiper
+            effect="coverflow"
+            className="swiper"
+            modules={[Navigation, Pagination, EffectCoverflow, Autoplay]}
+            slidesPerView={"auto"}
+            centeredSlides={true}
+            loop={true}
+            grabCursor={true}
+            coverflowEffect={{
+              rotate: 0,
+              stretch: 0,
+              depth: 300,
+              modifier: 1,
+            }}
+            autoplay={{
+              delay: 3000,
+              disableOnInteraction: false,
+            }}
+            pagination={{ el: ".custom-pagination" }}
+          >
+            {workshop.map((item) => (
+              <SwiperSlide
+                key={item.id}
+                style={{ width: "500px", height: "600px" }}
+              >
+                <div
+                  className={styles["workshop-card"]}
+                  style={{ backgroundImage: `url(${item.imageUrl})` }}
+                >
+                  <div className={styles["workshop-card-content"]}>
+                    <h3 className={styles["workshop-card-car-type"]}>
+                      {item.type}
+                    </h3>
+                    <h2 className={styles["workshop-card-car-title"]}>
+                      {item.title}
+                    </h2>
+                  </div>
+                </div>
+              </SwiperSlide>
+            ))}
+          </Swiper>
+          <div className="custom-pagination"></div>
+        </div>
+      </section>
     </div>
   );
 }
